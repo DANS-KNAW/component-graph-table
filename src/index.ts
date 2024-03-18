7d7b1bd6eadf7bb0bd95adc59ca937d17b5d10e7
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-03-18 16:27:40 +0100

    Add Elasticsearch integration and ingest data from PostgreSQL

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+
+import path from "path";
+import dotenv from "dotenv";
+import dbIngestJson from "./dbIngestJson";
+import esIngestIndex from "./esIngestIndex";
+
+dotenv.config({ path: path.join(__dirname, "../.env") });
+
+(async () => {
+  await dbIngestJson();
+  await esIngestIndex();
+})();
